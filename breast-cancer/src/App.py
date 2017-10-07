e952adfdd14c310a6250ae3094fd10d032ac132c
QIAO <qkywy2009@163.com>
2017-10-07 22:10:01 +0800

    finish breast-cancer

--- breast-cancer/src/App.py
+++ breast-cancer/src/App.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
@@ -27,12 +28,18 @@ if __name__ == '__main__':
         names=column_names)
     data = data.replace(to_replace='?', value=np.nan)
     data = data.dropna(how='any')
-    print(data.shape)
     X_train, X_test, y_train, y_test = train_test_split(
         data[column_names[1:10]], data[column_names[10]], test_size=0.25, random_state=33)
-    print(y_train.value_counts())
     ss = StandardScaler()
     X_train = ss.fit_transform(X_train)
     X_test = ss.transform(X_test)
     lr = LogisticRegression()
     sgdc = SGDClassifier()
+    lr.fit(X_train,y_train)
+    lr_y_predict=lr.predict(X_test)
+    sgdc.fit(X_train,y_train)
+    sgdc_y_predict=sgdc.predict(X_test)
+    print('Accuracy of LR Classifier :',lr.score(X_test,y_test))
+    print(classification_report(y_test,lr_y_predict,target_names=['Benign','Malignant']))
+    print('Accuracy of SGD Classifier :',sgdc.score(X_test,y_test))
+    print(classification_report(y_test,sgdc_y_predict,target_names=['Benign','Malignant']))
